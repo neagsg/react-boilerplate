e69729c560f86e1836669b137621f2a3662dca48
Gean Gomes <geansg4@gmail.com>
2021-05-11 11:04:47 -0300

    chore: add husky and lint-staged

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next.js na prática</title>
+      </Head>
+
+      <div>Hello World</div>
+    </div>
+  )
+}
