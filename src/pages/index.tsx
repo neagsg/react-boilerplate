bb8e848bf445681088dfceb61694fdc136e29d31
Gean Gomes <geansg4@gmail.com>
2021-05-12 13:05:24 -0300

    chore: add styled-components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,6 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Next.js na prática</title>
-      </Head>
-
       <div>Hello World</div>
     </div>
   )
